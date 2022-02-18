2a7788c4e80463d4b59ec7f0899d86047b092b42
Rizwan Abbasi <rizwan@rizwanabbasi.com>
2022-02-18 12:21:34 +0500

    Day3 Completed - Initial Route - Routes - MaterialApp() - ThemeData() - Style

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/home_page.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,7 +9,24 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: HomePage(),
+      //we are using / route so need to use home here
+      //home: HomePage(),
+      themeMode: ThemeMode.light,
+      //theme is for light theme
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      //this is for dark theme
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        //primarySwatch: Colors.red
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
