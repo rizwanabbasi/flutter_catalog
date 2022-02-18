42adfdb99002fc98441b47e96524246992b1194a
Rizwan Abbasi <rizwan@rizwanabbasi.com>
2022-02-18 14:57:19 +0500

    Day4 Completed 1. Add Images 2. Google Fonts 3. SizedBox() 4. TextFormField() 5. ElevatedButton()

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       //we are using / route so need to use home here
       //home: HomePage(),
@@ -15,16 +15,17 @@ class MyApp extends StatelessWidget {
       //theme is for light theme
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
       //this is for dark theme
       darkTheme: ThemeData(
         brightness: Brightness.dark,
         //primarySwatch: Colors.red
       ),
-      initialRoute: "/home",
+      initialRoute: "/login",
       routes: {
-        "/" : (context) => LoginPage(),
-        "/home" : (context) => HomePage(),
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
         "/login": (context) => LoginPage(),
       },
     );
